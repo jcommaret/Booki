0432460b5ad3b5bafd97a127bbc0374caef68cdc
Jérôme Commaret <jcommaret@gmail.com>
2025-04-22 16:55:22 +0200

    Suppression des fichiers de polices Raleway-Light et Raleway-Bold, mise à jour des styles CSS pour utiliser la police Raleway avec des poids spécifiques, et optimisation de la configuration Vite pour inclure la dépendance @fontsource/raleway.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-import "@fontsource/raleway/300.css"; // Light
-import "@fontsource/raleway/400.css"; // Regular
-import "@fontsource/raleway/700.css"; // Bold
-
+import '@fontsource/raleway/400.css'; // Light
+import '@fontsource/raleway/700.css'; // Bold
 
 import App from './App.tsx'
 
